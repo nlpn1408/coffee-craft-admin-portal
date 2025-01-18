46b2ea5d5a678912d138b52c526092c623c094ed
nlpn1408 <96246678+nlpn1408@users.noreply.github.com>
2025-01-18 18:46:12 +0700

    update code

--- src/state/api.ts
+++ src/state/api.ts
@@ -62,30 +62,30 @@ export const api = createApi({
   tagTypes: ["DashboardMetrics", "Products", "Users", "Expenses"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
-      query: () => "/dashboard",
+      query: () => "/product-service/dashboard",
       providesTags: ["DashboardMetrics"],
     }),
     getProducts: build.query<Product[], string | void>({
       query: (search) => ({
-        url: "/products",
+        url: "/product-service/products",
         params: search ? { search } : {},
       }),
       providesTags: ["Products"],
     }),
     createProduct: build.mutation<Product, NewProduct>({
       query: (newProduct) => ({
-        url: "/products",
+        url: "/product-service/products",
         method: "POST",
         body: newProduct,
       }),
       invalidatesTags: ["Products"],
     }),
     getUsers: build.query<User[], void>({
-      query: () => "/users",
+      query: () => "/product-service/users",
       providesTags: ["Users"],
     }),
     getExpensesByCategory: build.query<ExpenseByCategorySummary[], void>({
-      query: () => "/expenses",
+      query: () => "/product-service/expenses",
       providesTags: ["Expenses"],
     }),
   }),
